59e9c552576c0fb9f8771e5e78dbeaca5c78c52d
Darshan <darshan.gangurde@walmart.com>
2024-06-30 12:52:16 +0530

    update

---
+++ java_basics/CharPrint.java
@@ -0,0 +1,8 @@
+public class CharPrint{
+    public static void main(String [] args){
+        int a=67;
+        System.out.println(a);
+        char b =(char) a;
+        System.out.println(b);
+    }
+}
